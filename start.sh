37340b18237749594a0a1fd54c6a929ce18fafc8
Ben Kulbertis <ben@kulbertis.org>
2018-10-22 19:47:54 -0600

    Remove comment and switch bash to sh

--- start.sh
+++ start.sh
@@ -7,5 +7,4 @@ docker-compose exec kube ./init.sh
 docker-compose exec slate helm init --service-account tiller
 docker-compose exec slate pip install -r /opt/slate-portal/requirements.txt
 docker-compose exec slate sh -c "sed -i 's/localhost/0\.0\.0\.0/g' /opt/slate-portal/run_*"
-#docker-compose exec -d slate /bin/bash -c "cd /opt/slate-portal && ./run_service.py"
-docker-compose exec -d slate /bin/bash -c "cd /opt/slate-portal && ./run_portal.py"
+docker-compose exec -d slate /bin/sh -c "cd /opt/slate-portal && ./run_portal.py"
