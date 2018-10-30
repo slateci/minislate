e9b69ac360dcbcac908e7a492c85a4110841aa06
Ben Kulbertis <ben@kulbertis.org>
2018-10-30 16:21:37 -0600

    Fixed CoreDNS issue related to kubelet DNS.

--- dind/init.sh
+++ dind/init.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+# resolv.conf
+echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
+systemctl restart systemd-resolved
 systemctl restart kubelet
-kubeadm init --ignore-preflight-errors=all
+# Install Kubernetes
+kubeadm init --ignore-preflight-errors=all --pod-network-cidr=192.168.0.0/16
 export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/etcd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/rbac.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/calico.yaml
+# Remove master taint
+kubectl taint nodes --all node-role.kubernetes.io/master-
+# Calico
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/etcd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/rbac.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/calico.yaml
 kubectl rollout status ds/calico-node -n kube-system
 systemctl restart kubelet
-kubectl taint nodes --all node-role.kubernetes.io/master-
+# Tiller (Helm)
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
