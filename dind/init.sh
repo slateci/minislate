c48a84b235bfc0bf72e61d702a4d3c032084452c
Ben Kulbertis <ben@kulbertis.org>
2018-10-30 16:33:40 -0600

    Added fallback DNS

--- dind/init.sh
+++ dind/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# resolv.conf
+# DNS for kubelet
 echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
+echo "FallbackDNS=8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
 systemctl restart kubelet
 # Install Kubernetes
