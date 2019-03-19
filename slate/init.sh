d3247abd6b2189c695389adb6c8a5d663b62384c
Ben Kulbertis <ben@kulbertis.org>
2019-03-19 16:39:00 -0400

    Removed HostPath provisioner and set NFS provisioner to default

--- slate/init.sh
+++ slate/init.sh
@@ -4,7 +4,7 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 helm init --service-account tiller
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
-helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ stable/nfs-client-provisioner
+helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner
 pip install -r /opt/slate-portal/requirements.txt
 sed -i 's/localhost/0\.0\.0\.0/g' /opt/slate-portal/run_*
 cd /opt/slate-portal && ./run_portal.py &
