cfee5a378c20756495364ffe2fd25551495177b4
Ben Kulbertis <ben@kulbertis.org>
2019-03-20 17:19:14 -0400

    Notify user at end of init what their volume mounts are.

--- slate/init.sh
+++ slate/init.sh
@@ -10,5 +10,5 @@ sed -i 's/localhost/0\.0\.0\.0/g' /opt/slate-portal/run_*
 cd /opt/slate-portal && ./run_portal.py &
 slate group create ms-group --field 'Resource Provider'
 slate cluster create ms-c --group ms-group --org SLATE -y
-printf "\n=============================================================\n"
-printf "\n${bold}Default Group:${normal} ms-group\n${bold}Default Cluster:${normal} ms-c\n\n"
+printf "\n${bold}=============================================================\n${normal}"
+printf "\n${bold}Default Group:${normal} ms-group\n${bold}Default Cluster:${normal} ms-c\n"
