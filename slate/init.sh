b2424dcee03307340281f75b866417b1546b696f
cnweaver <cnweaver@gmail.com>
2019-07-16 17:55:20 -0500

    Use supervisord to run the portal at the same time as the API server

--- slate/init.sh
+++ slate/init.sh
@@ -6,8 +6,6 @@ helm init --service-account tiller
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
 helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner
 pip install -r /opt/slate-portal/requirements.txt
-sed -i 's/localhost/0\.0\.0\.0/g' /opt/slate-portal/run_*
-cd /opt/slate-portal && ./run_portal.py &
 slate group create ms-group --field 'Resource Provider'
 slate cluster create ms-c --group ms-group --org SLATE --no-ingress -y
 printf "\n${bold}=============================================================\n${normal}"
