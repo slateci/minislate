7e5ca95c0798c46d0c5b33762ae2ec6816a64744
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-07-26 13:17:14 -0600

    update default group and cluster names

--- slate/init.sh
+++ slate/init.sh
@@ -6,7 +6,7 @@ helm init --service-account tiller
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
 helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner
 pip install -r /opt/slate-portal/requirements.txt
-slate group create ms-group --field 'Resource Provider'
-slate cluster create ms-c --group ms-group --org SLATE --no-ingress -y
+slate group create my-group --field 'Resource Provider'
+slate cluster create my-cluster --group my-group --org SLATE --no-ingress -y
 printf "\n${bold}=============================================================\n${normal}"
-printf "\n${bold}Default Group:${normal} ms-group\n${bold}Default Cluster:${normal} ms-c\n"
+printf "\n${bold}Default Group:${normal} my-group\n${bold}Default Cluster:${normal} my-cluster\n"
