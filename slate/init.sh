540588d1375d75f26cae5250571bc65d1a63c427
Mansalu <mitchell.steinman@utah.edu>
2019-09-12 12:06:45 -0600

    move portal dependency installation to dockerfile

--- slate/init.sh
+++ slate/init.sh
@@ -5,7 +5,6 @@ normal=$(tput sgr0)
 helm init --service-account tiller
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
 helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner
-pip install -r /opt/slate-portal/requirements.txt
 slate group create my-group --field 'Resource Provider'
 slate cluster create my-cluster --group my-group --org SLATE --no-ingress -y
 printf "\n${bold}=============================================================\n${normal}"
