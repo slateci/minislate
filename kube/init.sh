6613eb33c884003c80e0fba61f2641e564b2ece4
Mansalu <mitchell.steinman@utah.edu>
2019-09-12 11:49:19 -0600

    remove yum install

--- kube/init.sh
+++ kube/init.sh
@@ -11,18 +11,6 @@ systemctl restart systemd-resolved
 # rm -f /hyperkube.tar
 
 # Try grabbing packages manually instead of hyperkube
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kube*
-EOF
-
-yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 systemctl enable --now kubelet
 
