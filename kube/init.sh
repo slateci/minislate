378e4db11dd3f8425b9b00be4260c0e4d1c17c41
Mansalu <mitchell.steinman@utah.edu>
2019-09-12 11:45:04 -0600

    remove hyperkube and install k8s with yum

--- kube/init.sh
+++ kube/init.sh
@@ -4,12 +4,29 @@ set -e
 echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
 echo "FallbackDNS=8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
-systemctl restart kubelet
+
 # Import cached hyperkube image
-docker pull k8s.gcr.io/hyperkube:v1.14.0 && docker save -o hyperkube.tar k8s.gcr.io/hyperkube:v1.14.0
-docker load -i /hyperkube.tar
-rm -f /hyperkube.tar
-# Install Kubernetes
+# docker pull k8s.gcr.io/hyperkube:v1.14.0 && docker save -o hyperkube.tar k8s.gcr.io/hyperkube:v1.14.0
+# docker load -i /hyperkube.tar
+# rm -f /hyperkube.tar
+
+# Try grabbing packages manually instead of hyperkube
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
+
+# Deploy Kubernetes
 kubeadm init --config config.yaml --ignore-preflight-errors=all
 # Remove master taint
 kubectl taint nodes --all node-role.kubernetes.io/master-
