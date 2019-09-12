b95ab991300015d9cae923f67fb1c274c91f0d83
Mansalu <mitchell.steinman@utah.edu>
2019-09-12 12:00:04 -0600

    cleanup removal of hyperkube

--- kube/init.sh
+++ kube/init.sh
@@ -4,16 +4,7 @@ set -e
 echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
 echo "FallbackDNS=8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
-
-# Import cached hyperkube image
-# docker pull k8s.gcr.io/hyperkube:v1.14.0 && docker save -o hyperkube.tar k8s.gcr.io/hyperkube:v1.14.0
-# docker load -i /hyperkube.tar
-# rm -f /hyperkube.tar
-
-# Try grabbing packages manually instead of hyperkube
-
 systemctl enable --now kubelet
-
 # Deploy Kubernetes
 kubeadm init --config config.yaml --ignore-preflight-errors=all
 # Remove master taint
