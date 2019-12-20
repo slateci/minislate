cfbe5b64d4d0e3cae0a91ba6802822fef4e0c3bc
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-20 13:07:36 -0700

    try without tiller

--- slate/init.sh
+++ slate/init.sh
@@ -2,8 +2,6 @@
 set -e
 bold=$(tput bold)
 normal=$(tput sgr0)
-helm init --service-account tiller
-kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
 helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner
 slate group create my-group --field 'Resource Provider'
 slate cluster create my-cluster --group my-group --org SLATE --no-ingress -y
