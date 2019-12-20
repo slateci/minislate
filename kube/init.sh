9f9830fc04863e18a857b02697f55f7184be9c52
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-20 10:58:46 -0700

    remove unneeded comments

--- kube/init.sh
+++ kube/init.sh
@@ -4,8 +4,6 @@ set -e
 echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
 echo "FallbackDNS=8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
-# Deploy Kubernetes
-# Remove master taint
 # Calico
 kubectl apply -f calico.yaml
 
