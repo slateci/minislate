2106be7b4096af7408fa8f75f61b57fdc1b984f8
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-20 10:54:22 -0700

    remove kubeadm pieces

--- kube/init.sh
+++ kube/init.sh
@@ -4,17 +4,10 @@ set -e
 echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
 echo "FallbackDNS=8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
-systemctl enable --now kubelet
 # Deploy Kubernetes
-kubeadm init --config config.yaml --ignore-preflight-errors=all
 # Remove master taint
-kubectl taint nodes --all node-role.kubernetes.io/master-
 # Calico
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/etcd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/rbac.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/calico.yaml
-kubectl rollout status ds/calico-node -n kube-system
-systemctl restart kubelet
+kubectl apply -f calico.yaml
 # Tiller (Helm)
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
