b6d485cffa14d523c1a140b5de9718e9c0594dfc
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-20 10:57:37 -0700

    remove tiller stuff

--- kube/init.sh
+++ kube/init.sh
@@ -8,6 +8,4 @@ systemctl restart systemd-resolved
 # Remove master taint
 # Calico
 kubectl apply -f calico.yaml
-# Tiller (Helm)
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
