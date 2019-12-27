718f113f89ac75f9b45acc3ab089c6907a6c11d2
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-27 09:32:44 -0700

    Update init.sh

--- kube/init.sh
+++ kube/init.sh
@@ -4,7 +4,9 @@ set -e
 echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
 echo "FallbackDNS=8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
+# K3s
+curl -sfL https://get.k3s.io | sh
 # Calico
 kubectl apply -f calico.yaml
-curl -sfL https://get.k3s.io | sh
+
 
