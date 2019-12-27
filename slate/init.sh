e4a1b0449add961c9ce8b433b1ff5fdb6d222ffa
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-27 11:06:17 -0700

    fix typo

--- slate/init.sh
+++ slate/init.sh
@@ -2,7 +2,7 @@
 set -e
 bold=$(tput bold)
 normal=$(tput sgr0)
-kubectl config set clusters.default.serverr https://kube:6443
+kubectl config set clusters.default.server https://kube:6443
 helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner
 slate group create my-group --field 'Resource Provider'
 slate cluster create my-cluster --group my-group --org SLATE --no-ingress -y
