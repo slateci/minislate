ad846316e3884e00409b9ff8993f03954d59ebdd
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-12-27 11:57:41 -0700

    use local-path provisioner not nfs

--- slate/init.sh
+++ slate/init.sh
@@ -3,8 +3,6 @@ set -e
 bold=$(tput bold)
 normal=$(tput sgr0)
 kubectl config set clusters.default.server https://kube:6443
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm install --namespace kube-system --set nfs.server=nfs --set nfs.path=/ --set storageClass.defaultClass=true stable/nfs-client-provisioner --generate-name
 slate group create my-group --field 'Resource Provider'
 slate cluster create my-cluster --group my-group --org SLATE --no-ingress -y
 printf "\n${bold}=============================================================\n${normal}"
