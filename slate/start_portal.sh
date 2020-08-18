94ffbf1b6b29cfd76599a87a5040c25e60647849
Mitchell <35324660+Mansalu@users.noreply.github.com>
2020-08-18 15:27:58 -0600

    set correct directory

--- slate/start_portal.sh
+++ slate/start_portal.sh
@@ -1,3 +1,5 @@
+cd /opt/slate-portal/
+
 PYTHONPATH=/opt/slate-portal/venv/lib64/python27.zip:/opt/slate-portal/venv/lib64/python2.7:/opt/slate-portal/venv/lib64/python2.7/plat-linux2:/opt/slate-portal/venv/lib64/python2.7/lib-tk:/opt/slate-portal/venv/lib64/python2.7/lib-old:/opt/slate-portal/venv/lib64/python2.7/lib-dynload:/usr/lib64/python2.7:/usr/lib/python2.7:/opt/slate-portal/venv/lib/python2.7/site-packages
 
 echo 'Creating portal Virtual Environment..'
