16a69dc58ddd31553e0fcd2b16f8ab80446d7667
Mitchell <35324660+Mansalu@users.noreply.github.com>
2020-08-18 15:11:19 -0600

    Update start_portal.sh

--- slate/start_portal.sh
+++ slate/start_portal.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+!#/bin/sh
 
 PYTHONPATH=/opt/slate-portal/venv/lib64/python27.zip:/opt/slate-portal/venv/lib64/python2.7:/opt/slate-portal/venv/lib64/python2.7/plat-linux2:/opt/slate-portal/venv/lib64/python2.7/lib-tk:/opt/slate-portal/venv/lib64/python2.7/lib-old:/opt/slate-portal/venv/lib64/python2.7/lib-dynload:/usr/lib64/python2.7:/usr/lib/python2.7:/opt/slate-portal/venv/lib/python2.7/site-packages
 
